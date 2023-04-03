e278e4a1aafbb667a9370186b5e67897b2f9a25b
ygkwnag <ygkwnag@gmail.com>
2023-04-03 09:58:56 +0900

    ai 연동 추가

--- src/controllers/openai.ts
+++ src/controllers/openai.ts
@@ -1,12 +1,11 @@
 import axios from 'axios';
 
-const OPENAI_API_KEY = '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';
 
 export async function generateChatMessage(query:string):Promise<string> {
     const url = 'https://api.openai.com/v1/chat/completions';
     const headers = {
         'Content-Type': 'application/json',
-        'Authorization': `Bearer ${OPENAI_API_KEY}`
+        'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`
     };
     const text_davinci = {
         model: 'text-davinci-002',
