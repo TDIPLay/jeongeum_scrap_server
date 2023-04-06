b34558fbebe1d5ee8957b4e06602ea4cf06b0fef
ygkwnag <ygkwnag@gmail.com>
2023-04-06 17:10:26 +0900

    commom 기본정보 추가

--- src/helpers/common.ts
+++ src/helpers/common.ts
@@ -1,15 +1,9 @@
 import { ResponseType } from 'axios';
 export const REDIS_EXPIRE = 86400		        // 레디스 expire TTL
 export const FIX = 10000		                    // 소수점 4자리
+export const RKEYWORD = "NewAllKeyword"		                    //Redis KEYWORD key
+export const MAX_LINK = 1000		                    // MAX_LINK
 
-export const QUERY = {
-    APP: "SELECT\n" +
-        "  a.*\n" +
-        "FROM\n" +
-        "  app a\n" +
-        "WHERE a.is_active = 1  "
-
-}
 
 export const DEVICE_STATUS = {
     active: 'Y',
@@ -17,8 +11,6 @@ export const DEVICE_STATUS = {
     unknown: 'UNKNOWN'
 }
 
-export const emailRegex = /\S+@\S+\.\S+/;
-export const User_Agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.64 Safari/537.36";
 export const AXIOS_OPTIONS = {
     headers: {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.64 Safari/537.36",
